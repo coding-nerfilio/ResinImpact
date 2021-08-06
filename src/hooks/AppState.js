21de7e69b47e5ab281a6f963200eade3264fd3e7
coding-nerfilio <frisso2001@gmail.com>
2021-08-06 18:29:07 -0300

    Add AppState hook

---
+++ src/hooks/AppState.js
@@ -0,0 +1,29 @@
+import {useEffect, useRef, useState} from 'react';
+import {AppState} from 'react-native';
+
+export const AS = () => {
+  const appState = useRef(AppState.currentState);
+  const [appStateVisible, setAppStateVisible] = useState(appState.current);
+
+  useEffect(() => {
+    AppState.addEventListener('change', _handleAppStateChange);
+
+    return () => {
+      AppState.removeEventListener('change', _handleAppStateChange);
+    };
+  }, []);
+
+  const _handleAppStateChange = nextAppState => {
+    if (
+      appState.current.match(/inactive|background/) &&
+      nextAppState === 'active'
+    ) {
+      console.log('App has come to the foreground!');
+    }
+
+    appState.current = nextAppState;
+    setAppStateVisible(appState.current);
+    console.log('AppState', appState.current);
+  };
+  return {appStateVisible};
+};
