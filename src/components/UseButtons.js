b52bd8c252b50a767c086e3453bc98c0f8ff2a46
coding-nerfilio <frisso2001@gmail.com>
2021-08-05 23:57:41 -0300

    Add resin decreasement function on UseButtons

--- src/components/UseButtons.js
+++ src/components/UseButtons.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import {View, TouchableOpacity, Text} from 'react-native';
 
-const UseButtons = () => {
+const UseButtons = ({decreaseResin}) => {
   return (
     <View style={{flexDirection: 'row'}}>
-      <TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          decreaseResin(0);
+        }}>
         <Text>-20</Text>
       </TouchableOpacity>
-      <TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          decreaseResin(1);
+        }}>
         <Text>-40</Text>
       </TouchableOpacity>
     </View>
