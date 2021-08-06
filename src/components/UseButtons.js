d5b912a784dd6d82e30881815ed517ec92168cf9
coding-nerfilio <frisso2001@gmail.com>
2021-08-06 18:22:25 -0300

    Add more options to UseButtons component

--- src/components/UseButtons.js
+++ src/components/UseButtons.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import {View, TouchableOpacity, Text} from 'react-native';
 
-const UseButtons = ({decreaseResin}) => {
+const UseButtons = ({useResin}) => {
   return (
     <View style={{flexDirection: 'row'}}>
       <TouchableOpacity
         onPress={() => {
-          decreaseResin(0);
+          useResin(0);
+        }}>
+        <Text>-60</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          useResin(1);
+        }}>
+        <Text>-40</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          useResin(2);
+        }}>
+        <Text>-30</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          useResin(3);
         }}>
         <Text>-20</Text>
       </TouchableOpacity>
       <TouchableOpacity
         onPress={() => {
-          decreaseResin(1);
+          useResin(4);
         }}>
-        <Text>-40</Text>
+        <Text>+60</Text>
       </TouchableOpacity>
     </View>
   );
