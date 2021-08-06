2ebfd847c36a24575fee1306c9b7fabce7c0434e
coding-nerfilio <frisso2001@gmail.com>
2021-08-06 18:19:29 -0300

    Refactor decreaseResin

--- src/hooks/Resin.js
+++ src/hooks/Resin.js
@@ -11,13 +11,22 @@ const Resin = initialResin => {
     setResin(newValue);
   };
 
-  const decreaseResin = i => {
+  const useResin = i => {
     switch (i) {
       case 0:
-        setResin(resinAmount >= 20 ? resinAmount - 20 : 0);
+        setResin(resinAmount >= 60 ? resinAmount - 60 : resinAmount);
         break;
       case 1:
-        setResin(resinAmount >= 40 ? resinAmount - 40 : 0);
+        setResin(resinAmount >= 40 ? resinAmount - 40 : resinAmount);
+        break;
+      case 2:
+        setResin(resinAmount >= 30 ? resinAmount - 30 : resinAmount);
+        break;
+      case 3:
+        setResin(resinAmount >= 20 ? resinAmount - 20 : resinAmount);
+        break;
+      case 4:
+        setResin(resinAmount + 60);
         break;
     }
   };
@@ -33,7 +42,7 @@ const Resin = initialResin => {
     return () => clearInterval(interval);
   }, []);
 
-  return {secondsLeft, resinAmount, modifyResin, decreaseResin};
+  return {secondsLeft, resinAmount, modifyResin, useResin};
 };
 
 export default Resin;
