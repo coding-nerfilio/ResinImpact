cf728d67840c85e2892a398de673445f736939b6
coding-nerfilio <frisso2001@gmail.com>
2021-08-06 00:06:14 -0300

    Add MAX_RESIN constant

--- src/components/ResinCounter.js
+++ src/components/ResinCounter.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {MAX_RESIN} from '../constants';
 
 const ResinCounter = ({resin}) => {
   return (
     <View style={{flexDirection: 'row'}}>
       <Text>{resin}</Text>
-      <Text>/ 160</Text>
+      <Text>/ {MAX_RESIN}</Text>
     </View>
   );
 };
