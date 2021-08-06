7ee0add557ff4d1dde01080cc1c50c8f13a0ff7f
coding-nerfilio <frisso2001@gmail.com>
2021-08-06 00:30:56 -0300

    Add second counting to Resin hook

--- src/hooks/Resin.js
+++ src/hooks/Resin.js
@@ -1,7 +1,11 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import {MAX_RESIN} from '../constants';
 
 const Resin = initialResin => {
   const [resinAmount, setResin] = useState(initialResin);
+  const [secondsLeft, setSecondsLeft] = useState(
+    (MAX_RESIN - initialResin) * 480,
+  );
 
   const modifyResin = newValue => {
     setResin(newValue);
@@ -18,7 +22,18 @@ const Resin = initialResin => {
     }
   };
 
-  return {resinAmount, modifyResin, decreaseResin};
+  useEffect(() => {
+    setSecondsLeft((MAX_RESIN - resinAmount) * 480);
+  }, [resinAmount]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSecondsLeft(secondsLeft => (secondsLeft > 0 ? secondsLeft - 1 : 0));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return {secondsLeft, resinAmount, modifyResin, decreaseResin};
 };
 
 export default Resin;
