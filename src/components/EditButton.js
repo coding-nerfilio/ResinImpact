c0064358ffc00cf5638791b883b376b0d1737cd5
coding-nerfilio <frisso2001@gmail.com>
2021-08-05 23:42:44 -0300

    Add edit button

---
+++ src/components/EditButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
+
+const EditButton = () => {
+  return (
+    <View>
+      <TouchableOpacity>
+        <Text>E</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default EditButton;
