45af2430953e278fef9026af39a428aa92897116
coding-nerfilio <frisso2001@gmail.com>
2021-08-15 20:39:33 -0300

    Rework of the old component

---
+++ src/components/EditResin.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import {Modal, Text, TextInput, TouchableOpacity} from 'react-native';
+
+const EditResin = modifyResin => {
+  const [state, setState] = useState({visible: false, value: ''});
+
+  const onValueChange = t => setState({...state, value: t});
+
+  const handleVisibility = () => setState({value: '', visible: !state.visible});
+
+  const handleModify = () => modifyResin(state.value);
+
+  return {
+    component: (
+      <Modal visible={state.visible}>
+        <TextInput
+          value={state.value}
+          onChangeText={onValueChange}
+          placeholder="Enter value"
+        />
+        <TouchableOpacity
+          onPress={() => {
+            handleModify();
+            handleVisibility();
+          }}>
+          <Text>Ok</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            handleVisibility();
+          }}>
+          <Text>Cancel</Text>
+        </TouchableOpacity>
+      </Modal>
+    ),
+    handleVisibility,
+  };
+};
+
+export default EditResin;
