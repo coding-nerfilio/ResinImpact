ca858e65c4e183adebdb28c376343d15e7949561
coding-nerfilio <frisso2001@gmail.com>
2021-08-15 20:41:20 -0300

    Add EditResin component

--- src/components/ResinCounter.js
+++ src/components/ResinCounter.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import {MAX_RESIN} from '../constants';
+import EditResin from './EditResin';
 import ProgressBar from './ProgressBar';
 
-const ResinCounter = ({resin}) => {
+const ResinCounter = ({resin, modifyResin}) => {
+  const ModifyResin = EditResin(modifyResin);
+
   return (
-    <View style={{flexDirection: 'column'}}>
+    <TouchableOpacity
+      onPress={ModifyResin.handleVisibility}
+      style={{flexDirection: 'column', alignItems: 'center'}}>
       <View style={{flexDirection: 'row'}}>
         <Text>{resin}</Text>
         <Text>/{MAX_RESIN}</Text>
       </View>
       <ProgressBar resinAmount={resin} />
-    </View>
+      {ModifyResin.component}
+    </TouchableOpacity>
   );
 };
 
