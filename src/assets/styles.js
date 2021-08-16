63a093faf0acfe8e328e760e12d0cb67454a7562
coding-nerfilio <frisso2001@gmail.com>
2021-08-15 21:33:17 -0300

    Add styles file

---
+++ src/assets/styles.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+const Styles = StyleSheet.create({
+  Button: {
+    borderColor: 'black',
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    paddingHorizontal: 3,
+    paddingVertical: 2,
+  },
+});
+
+export default Styles;
