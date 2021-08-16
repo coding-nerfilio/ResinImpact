329d190211292b167027e205e04ca327d6e8d451
coding-nerfilio <frisso2001@gmail.com>
2021-08-15 22:20:44 -0300

    Add fade animation for AvailableOn component

--- src/components/TimeCounter.js
+++ src/components/TimeCounter.js
@@ -1,23 +1,35 @@
 import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
+import {Animated, Text} from 'react-native';
 import Strings from '../assets/strings';
+import AnimatedFade from '../hooks/AnimatedFade';
 import {fullOnTime, secondsToFullTime} from '../utils/Time';
 
 const TimeCounter = ({seconds}) => {
   const [timeLeft, setTimeLeft] = useState(secondsToFullTime(seconds));
 
   useEffect(() => {
-    setTimeLeft(secondsToFullTime(seconds));
+    if (seconds >= 0) {
+      setTimeLeft(secondsToFullTime(seconds));
+    }
   }, [seconds]);
 
   return (
-    <View style={{alignItems: 'center'}}>
+    <Animated.View style={{alignItems: 'center'}}>
       <Text style={{fontSize: 20, marginBottom: 10}}>
         {timeLeft.hours + ':' + timeLeft.minutes + ':' + timeLeft.seconds}
       </Text>
+      <AvailableOn seconds={seconds} />
+    </Animated.View>
+  );
+};
+
+const AvailableOn = ({seconds}) => {
+  const opacity = AnimatedFade(seconds);
+  return (
+    <Animated.View style={{alignItems: 'center', opacity}}>
       <Text>{Strings[0][0]}</Text>
       <Text>{fullOnTime(seconds)}</Text>
-    </View>
+    </Animated.View>
   );
 };
 
